8adf06eade38b4e51afeef561c4b727a24da5fd0
Olity <olity1712@gmail.com>
2023-04-03 15:15:42 +0600

    add Screen component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Screen from "./components/sidebar/Screen";
+
+
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Screen/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/sidebar/Screen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './screen.scss';
+
+function Screen() {
+    return (
+        <div className="screen_container">
+            <div className="screen_bg">
+                <span className="screen_dig">0</span>
+            </div>
+        </div>
+    );
+}
+
+export default Screen;
