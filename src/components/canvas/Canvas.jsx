d8da2089800bd89111f5dab858d845c3c4543c40
Olity <olity1712@gmail.com>
2023-04-12 13:17:32 +0600

    styled Canvas component

--- src/components/canvas/Canvas.jsx
+++ src/components/canvas/Canvas.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './canvas.scss';
+import {ReactComponent as AddIcon} from "../../images/Group 2.svg"
 
 function Canvas() {
     return (
-        <div className="canvas_container"></div>
+        <div className="canvas_container">
+            <AddIcon />
+            <p className="main_text">Перетащите сюда</p>
+            <p className="text">любой элемент <br/>из левой панели</p>
+        </div>
     );
 }
 
